461aa70a7471ef2a3952da0810776f4bfac39f40
fidashn <87206839+fidashn@users.noreply.github.com>
2022-06-16 02:15:28 +0300

    worked on the todoList component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function TodoForm(props) {
   const [input, setInput] = useState("");
 
   const handleChange = e => {
     setInput(e.target.value)
-  }
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input
-    // });
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
 
     setInput('')
-  }
+  };
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
